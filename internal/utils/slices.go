d68dbfd3367ec34bdad2f2fa3728676b80bc8ec4
Efstathios Chouliaris <Efstathios.Chouliaris@thomsonreuters.com>
2025-12-04 14:44:03 -0600

    slice utils

--- internal/utils/slices.go
+++ internal/utils/slices.go
@@ -1,5 +1,12 @@
 package utils
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+)
+
 // SlicesEqual checks if two slices contain the same elements (order independent)
 func SlicesEqual(a, b []string) bool {
 	if len(a) != len(b) {
@@ -23,12 +30,193 @@ func SlicesEqual(a, b []string) bool {
 	return true
 }
 
-// SliceContains checks if a slice contains a specific string
-func SliceContains(slice []string, item string) bool {
-	for _, v := range slice {
-		if v == item {
+func MapContainsKey[K comparable, V any](pattern K, input map[K]V) (K, bool) {
+	var (
+		found K
+		ok    bool
+	)
+	for k := range input {
+		ok = k == pattern
+		if ok {
+			found = k
+			break
+		}
+	}
+
+	return found, ok
+}
+
+// MapKeys Generic Function to take any map and return an array of keys
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+// MapValues Generic Function to take any map and return an array of values
+func MapValues[K comparable, V any](m map[K]V) []V {
+	r := make([]V, 0, len(m))
+	for _, v := range m {
+		r = append(r, v)
+	}
+	return r
+}
+
+// FindKeyInMap partial matches string to map keys and returns the actual key
+func FindKeyInMap[V any](pattern string, input map[string]V) (string, bool) {
+	for key := range input {
+		if strings.Contains(strings.ToLower(key), strings.ToLower(pattern)) {
+			return key, true
+		}
+	}
+
+	return "", false
+}
+
+// FindIndex finds the index of an element in a slice
+func FindIndex[T any](elems []T, predicate func(T) bool) int {
+	for i, elem := range elems {
+		if predicate(elem) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Find finds the first element in a slice that satisfies the predicate
+func Find[T any](elems []T, predicate func(T) bool) (T, bool) {
+	var out T
+	for _, elem := range elems {
+		if predicate(elem) {
+			return elem, true
+		}
+	}
+	return out, false
+}
+
+// IndexOf gets the index of an element (comparables only)
+func IndexOf[T comparable](elems []T, lookup T) int {
+	return FindIndex(elems, func(elem T) bool {
+		return elem == lookup
+	})
+}
+
+// SliceContains checks whether or not an element exists (comparables only)
+func SliceContains[T comparable](elems []T, lookup T) bool {
+	return IndexOf(elems, lookup) > -1
+}
+
+// MapSlice takes a slice and applies a function to each element to output a new slice of a different type
+func MapSlice[T any, K any](elems []T, mapper func(T) K) []K {
+	var out []K
+	for _, elem := range elems {
+		out = append(out, mapper(elem))
+	}
+	return out
+}
+
+// ReduceSlice takes a slice and applies a reducer function to each element to output a single value
+// assumes the destination is initialized
+func ReduceSlice[T any, K any](elems []T, reducer func(K, T) K, dest K) K {
+	out := dest
+
+	for _, elem := range elems {
+		out = reducer(out, elem)
+	}
+	return out
+}
+
+// Every checks if all elements in a slice satisfy a predicate
+func Every[T any](elems []T, predicate func(T) bool) bool {
+	for _, elem := range elems {
+		if !predicate(elem) {
+			return false
+		}
+	}
+	return true
+}
+
+// Some checks if any element in a slice satisfies a predicate
+func Some[T any](elems []T, predicate func(T) bool) bool {
+	for _, elem := range elems {
+		if predicate(elem) {
 			return true
 		}
 	}
 	return false
+}
+
+func FilterSlice[T any](elems []T, filter func(T) bool) []T {
+	var filteredResult []T
+	for _, elem := range elems {
+		if filter(elem) {
+			filteredResult = append(filteredResult, elem)
+		}
+	}
+
+	return filteredResult
+}
+
+// UnorderedSliceRemove takes a slice and removes the element at the specified index. Does not maintain order (FAST).
+func UnorderedSliceRemove[T any](ctx context.Context, elems []T, index int) []T {
+	if index >= len(elems) {
+		fmt.Fprintf(os.Stderr, "index out of range: %d", index)
+		return elems
+	}
+
+	elems[index] = elems[len(elems)-1]
+	return elems[:len(elems)-1]
+}
+
+// SliceRemove takes a slice and removes the element at the specified index. Maintains order (SLOW).
+func SliceRemove[T any](ctx context.Context, elems []T, index int) []T {
+	if index >= len(elems) {
+		fmt.Fprintf(os.Stderr, "index out of range: %d", index)
+		return elems
+	}
+
+	return append(elems[:index], elems[index+1:]...)
+}
+
+// SliceUnique takes a slice of strings or integers and returns a slice containing only unique values
+func SliceUnique[T string | int](elems []T) []T {
+	allKeys := make(map[T]bool)
+	uniqueElems := []T{}
+	for _, elem := range elems {
+		if _, value := allKeys[elem]; !value {
+			allKeys[elem] = true
+			uniqueElems = append(uniqueElems, elem)
+		}
+	}
+	return uniqueElems
+}
+
+// SliceDifference takes two slices and returns the elements that exist in one but not the other
+func SliceDifference[T comparable](a, b []T) []T {
+	set := make(map[T]bool)
+	for _, item := range b {
+		set[item] = true
+	}
+	diff := []T{}
+	for _, item := range a {
+		if _, value := set[item]; !value {
+			diff = append(diff, item)
+		}
+	}
+	return diff
+}
+
+// ChunkSlice takes a slice and return chunks of slice by given size.
+func ChunkSlice[T any](items []T, size int) [][]T {
+	var chunks [][]T
+	for i := 0; i < len(items); i += size {
+		end := i + size
+		if end > len(items) {
+			end = len(items)
+		}
+		chunks = append(chunks, items[i:end])
+	}
+	return chunks
 }
--- internal/utils/slices_test.go
+++ internal/utils/slices_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"context"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -104,4 +107,350 @@ func TestSliceContains(t *testing.T) {
 			assert.Equal(t, tt.expected, result, "SliceContains(, ) = %v %v", tt.slice, tt.item)
 		})
 	}
+}
+
+func TestFilterSlice(t *testing.T) {
+	slice := []string{"one", "two", "three", "four", "five"}
+	expected := []string{"one", "three", "five"}
+
+	actual := FilterSlice(slice, func(s string) bool {
+		return strings.HasSuffix(s, "e")
+	})
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestFindIndex(t *testing.T) {
+	slice := []string{"one", "two", "three", "four", "five"}
+
+	t.Run("found", func(t *testing.T) {
+		idx := FindIndex(slice, func(s string) bool {
+			return s == "three"
+		})
+
+		assert.Equal(t, 2, idx)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		idx := FindIndex(slice, func(s string) bool {
+			return s == "six"
+		})
+
+		assert.Equal(t, -1, idx)
+	})
+}
+
+func TestFind(t *testing.T) {
+	slice := []string{"one", "two", "three", "four", "five"}
+
+	t.Run("found", func(t *testing.T) {
+		found, ok := Find(slice, func(s string) bool {
+			return s == "three"
+		})
+
+		assert.True(t, ok)
+		assert.Equal(t, "three", found)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		found, ok := Find(slice, func(s string) bool {
+			return s == "six"
+		})
+
+		assert.False(t, ok)
+		assert.Equal(t, "", found)
+	})
+}
+
+func TestMapSlice(t *testing.T) {
+	type complexType struct {
+		Value string
+	}
+
+	slice := []complexType{
+		{Value: "one"},
+		{Value: "two"},
+		{Value: "three"},
+		{Value: "four"},
+		{Value: "five"},
+	}
+
+	actual := MapSlice(slice, func(c complexType) string {
+		return c.Value
+	})
+
+	expected := []string{"one", "two", "three", "four", "five"}
+	assert.Equal(t, expected, actual)
+}
+
+func TestEvery(t *testing.T) {
+	slice := []string{"one", "two", "three", "four", "five"}
+
+	t.Run("every", func(t *testing.T) {
+		every := Every(slice, func(s string) bool {
+			return strings.HasSuffix(s, "e")
+		})
+
+		assert.False(t, every)
+	})
+
+	t.Run("every - empty slice", func(t *testing.T) {
+		every := Every([]string{}, func(s string) bool {
+			return strings.HasSuffix(s, "e")
+		})
+
+		assert.True(t, every)
+	})
+}
+
+func TestSome(t *testing.T) {
+	slice := []string{"one", "two", "three", "four", "five"}
+
+	t.Run("some", func(t *testing.T) {
+		some := Some(slice, func(s string) bool {
+			return strings.HasSuffix(s, "e")
+		})
+
+		assert.True(t, some)
+	})
+
+	t.Run("some - empty slice", func(t *testing.T) {
+		some := Some([]string{}, func(s string) bool {
+			return strings.HasSuffix(s, "e")
+		})
+
+		assert.False(t, some)
+	})
+}
+
+func TestReduceSlice(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+
+	t.Run("sum", func(t *testing.T) {
+		sum := ReduceSlice(slice, func(acc, val int) int {
+			return acc + val
+		}, 0)
+
+		assert.Equal(t, 15, sum)
+	})
+
+	t.Run("product", func(t *testing.T) {
+		product := ReduceSlice(slice, func(acc, val int) int {
+			return acc * val
+		}, 1)
+
+		assert.Equal(t, 120, product)
+	})
+
+	t.Run("complex type as a destination", func(t *testing.T) {
+		type complexType struct {
+			Value int
+		}
+
+		sum := ReduceSlice(slice, func(acc complexType, val int) complexType {
+			acc.Value += val
+			return acc
+		}, complexType{})
+
+		assert.Equal(t, 15, sum.Value)
+	})
+}
+
+func TestSliceUnique(t *testing.T) {
+	t.Run("unique integers", func(t *testing.T) {
+		input := []int{1, 2, 2, 3, 4, 4, 5}
+		expected := []int{1, 2, 3, 4, 5}
+		actual := SliceUnique(input)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("unique strings", func(t *testing.T) {
+		input := []string{"apple", "banana", "apple", "cherry", "banana"}
+		expected := []string{"apple", "banana", "cherry"}
+		actual := SliceUnique(input)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		input := []int{}
+		expected := []int{}
+		actual := SliceUnique(input)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("all unique elements", func(t *testing.T) {
+		input := []string{"apple", "banana", "cherry"}
+		expected := []string{"apple", "banana", "cherry"}
+		actual := SliceUnique(input)
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestMapContainsKey(t *testing.T) {
+	t.Run("string keys", func(t *testing.T) {
+		m := map[string]int{
+			"one":   1,
+			"two":   2,
+			"three": 3,
+		}
+		key, found := MapContainsKey("two", m)
+		assert.True(t, found)
+		assert.Equal(t, "two", key)
+
+		key, found = MapContainsKey("four", m)
+		assert.False(t, found)
+		assert.Equal(t, "", key)
+	})
+
+	t.Run("struct keys", func(t *testing.T) {
+		type Key struct {
+			ID   int
+			Name string
+		}
+		type Value struct {
+			Age int
+		}
+		m := map[Key]Value{
+			{ID: 1, Name: "Alice"}: {Age: 30},
+			{ID: 2, Name: "Bob"}:   {Age: 25},
+		}
+		key, found := MapContainsKey(Key{ID: 1, Name: "Alice"}, m)
+		assert.True(t, found)
+		assert.Equal(t, Key{ID: 1, Name: "Alice"}, key)
+
+		key, found = MapContainsKey(Key{ID: 3, Name: "Charlie"}, m)
+		assert.False(t, found)
+		assert.Equal(t, Key{}, key)
+	})
+}
+
+func TestMapKeys(t *testing.T) {
+	t.Run("string keys", func(t *testing.T) {
+		m := map[string]int{
+			"one":   1,
+			"two":   2,
+			"three": 3,
+		}
+		keys := MapKeys(m)
+		expected := []string{"one", "two", "three"}
+		sort.Strings(keys)
+		sort.Strings(expected)
+		assert.Equal(t, expected, keys)
+	})
+
+	t.Run("struct keys", func(t *testing.T) {
+		type Key struct {
+			ID   int
+			Name string
+		}
+		type Value struct {
+			Age int
+		}
+		m := map[Key]Value{
+			{ID: 1, Name: "Alice"}: {Age: 30},
+			{ID: 2, Name: "Bob"}:   {Age: 25},
+		}
+		keys := MapKeys(m)
+		expected := []Key{
+			{ID: 1, Name: "Alice"},
+			{ID: 2, Name: "Bob"},
+		}
+		assert.ElementsMatch(t, expected, keys)
+	})
+}
+
+func TestMapValues(t *testing.T) {
+	t.Run("string values", func(t *testing.T) {
+		m := map[string]int{
+			"one":   1,
+			"two":   2,
+			"three": 3,
+		}
+		values := MapValues(m)
+		expected := []int{1, 2, 3}
+		sort.Ints(values)
+		sort.Ints(expected)
+		assert.Equal(t, expected, values)
+	})
+
+	t.Run("struct values", func(t *testing.T) {
+		type Key struct {
+			ID   int
+			Name string
+		}
+		type Value struct {
+			Age int
+		}
+		m := map[Key]Value{
+			{ID: 1, Name: "Alice"}: {Age: 30},
+			{ID: 2, Name: "Bob"}:   {Age: 25},
+		}
+		values := MapValues(m)
+		expected := []Value{
+			{Age: 30},
+			{Age: 25},
+		}
+		assert.ElementsMatch(t, expected, values)
+	})
+}
+
+func TestFindKeyInMap(t *testing.T) {
+	t.Run("string keys", func(t *testing.T) {
+		m := map[string]int{
+			"one":   1,
+			"two":   2,
+			"three": 3,
+		}
+		key, found := FindKeyInMap("tw", m)
+		assert.True(t, found)
+		assert.Equal(t, "two", key)
+
+		key, found = FindKeyInMap("four", m)
+		assert.False(t, found)
+		assert.Equal(t, "", key)
+	})
+}
+
+func TestSliceRemove(t *testing.T) {
+	ctx := context.Background()
+	generateTestSlice := func() []string {
+		return []string{"one", "two", "three", "four", "five"}
+	}
+
+	const testIndex = 2
+
+	t.Run("Ordered Remove", func(t *testing.T) {
+		slice := generateTestSlice()
+		expected := []string{"one", "two", "four", "five"}
+
+		actual := SliceRemove(ctx, slice, testIndex)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Ordered Remove - Out of bounds", func(t *testing.T) {
+		slice := generateTestSlice()
+		expected := generateTestSlice()
+
+		actual := SliceRemove(ctx, slice, len(slice))
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Unordered Remove", func(t *testing.T) {
+		slice := generateTestSlice()
+		removed := slice[testIndex]
+
+		assert.Equal(t, SliceContains(slice, removed), true)
+
+		actual := UnorderedSliceRemove(ctx, slice, testIndex)
+
+		assert.Equal(t, SliceContains(actual, removed), false)
+	})
+
+	t.Run("Unordered Remove - Out of bounds", func(t *testing.T) {
+		slice := generateTestSlice()
+		expected := generateTestSlice()
+
+		actual := UnorderedSliceRemove(ctx, slice, len(slice))
+		assert.Equal(t, expected, actual)
+	})
 }
